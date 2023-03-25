73a651b9976c530b0813b26fa8c54ff244c909a5
conypizango <122838163+conypizango@users.noreply.github.com>
2023-03-24 20:04:20 -0500

    menu

---
+++ perfil.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    const firebaseConfig = {
+        apiKey: "AIzaSyB2yJ0qYoEbQFcFjyV2IQnm7aKo3kfON-8",
+        authDomain: "light-d6705.firebaseapp.com",
+        projectId: "light-d6705",
+        storageBucket: "light-d6705.appspot.com",
+        messagingSenderId: "201259592460",
+        appId: "1:201259592460:web:cf6d62b1450170aeb53729"
+      };
+      // Initialize Firebase
+      const  aplicación  =  base de fuego.initializeApp( firebaseConfig ) ;
+    consola_registro ( aplicación ) ;
+
+});
